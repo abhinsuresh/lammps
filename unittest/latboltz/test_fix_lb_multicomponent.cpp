277994173659eedca613a3a3f18749887fd33f7d
Abhin Suresh <abhinsuresh01@gmail.com>
2026-08-25 15:53:29 -0400

    moved basic unit/smoke test from force-style to folder latboltz

--- unittest/latboltz/test_fix_lb_multicomponent.cpp
+++ unittest/latboltz/test_fix_lb_multicomponent.cpp
@@ -1,7 +1,9 @@
 #include "gtest/gtest.h"
 
+#include "fix.h"
 #include "input.h"
 #include "lammps.h"
+#include "modify.h"
 
 #include <mpi.h>
 
@@ -9,11 +11,12 @@
 
 using namespace LAMMPS_NS;
 
-TEST(FixLBMulticomponent, plain)
+
+LAMMPS *init_lammps()
 {
     const char *args[] = {"FixLBMulticomponent", "-log", "none", 
                           "-echo", "screen", "-nocite"};
-
+    
     char **argv = (char **)args;
     int argc = sizeof(args) / sizeof(char *);
 
@@ -26,18 +29,32 @@ TEST(FixLBMulticomponent, plain)
     
     command("region fluid block -16 16 -16 16 -3 3");
     command("create_box 0 fluid");
-
     command("timestep 1.0");
+    command("fix mcmp all lb/multicomponent "
+            "1 0.166667 1.0 D3Q19 dx 1 "
+            "C1 0.333333 C2 0.333333 C3 0.333334 "
+            "kappa1 0.01 kappa2 0.02 kappa3 0.05 "
+            "init mixture");
 
-    command(
-        "fix mcmp all lb/multicomponent "
-        "1 0.166667 1.0 D3Q19 dx 1 "
-        "C1 0.333333 C2 0.333333 C3 0.333334 "
-        "kappa1 0.01 kappa2 0.02 kappa3 0.05 "
-        "init mixture"
-    );
+    return lmp;
+}
 
-    command("run 10");
+void run_lammps(LAMMPS *lmp)
+{
+    lmp->input->one("run 10");
+} 
+
+TEST(FixLBMulticomponent, plain)
+{
+
+    LAMMPS *lmp = init_lammps();
+
+    auto fixes = lmp->modify->get_fix_by_style("lb/multicomponent");    
+        
+    ASSERT_EQ(fixes.size(), 1);
+    ASSERT_NE(fixes[0], nullptr);
+
+    run_lammps(lmp); 
 
     delete lmp;
 }
