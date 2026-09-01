823143d44e71c3e9debba0fb71824c5fcbd3d97b
Abhin Suresh <abhinsuresh01@gmail.com>
2026-09-01 15:18:23 -0400

    added condition to check pkg before compiling and adding test

--- unittest/latboltz/test_fix_lb_multicomponent.cpp
+++ unittest/latboltz/test_fix_lb_multicomponent.cpp
@@ -1,19 +1,38 @@
-#include "gtest/gtest.h"
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://www.lammps.org/, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------------
+ Contributing authors: Abhin Suresh (University of Delaware)
+------------------------------------------------------------------------- */
+
+// unit tests for fix style multicomponent fluid dynamics
+
 #include "test_main.h"
 #include "yaml_writer.h"
 
+#include "gtest/gtest.h"
+
+#include "input.h"
 #include "fix.h"
 #include "fix_lb_multicomponent.h"
-#include "input.h"
 #include "lammps.h"
 #include "modify.h"
 
-#include <mpi.h>
 #include <iostream>
+#include <mpi.h>
 #include <string>
 
 using namespace LAMMPS_NS;
-
 
 LAMMPS *init_lammps()
 {
@@ -26,9 +45,8 @@ LAMMPS *init_lammps()
     LAMMPS *lmp = new LAMMPS(argc, argv, MPI_COMM_WORLD);
 
     // utility lambda to improve readability
-    auto command = [&](const std::string &line)
-    {
-        lmp->input->one(line);
+    auto command = [&](const std::string &line){
+        lmp->input->one(line); 
     };
     
     command("region fluid block -16 16 -16 16 -3 3");
@@ -61,30 +79,47 @@ void generate_yaml_file(const char *outfile, const TestConfig &config)
 
 TEST(FixLBMulticomponent, plain)
 {
-
     LAMMPS *lmp = init_lammps();
     
+    // Check for LAMMPS initailization with the fix, else skip the test
+    if (!lmp) {                                                       
+      std::cerr << "One or more prerequisite styles are not available "
+                   "in this LAMMPS configuration:\n";               
+      for (auto &prerequisite : test_config.prerequisites)         
+        std::cerr << prerequisite.first << "_style " << prerequisite.second << "\n";
+                                                                    
+      GTEST_SKIP();                                                 
+    }                                                                 
+                                                                   
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    // get the base-class Fix * matching the style
     auto fixes = lmp->modify->get_fix_by_style("lb/multicomponent");    
         
     ASSERT_EQ(fixes.size(), 1);
     ASSERT_NE(fixes[0], nullptr);
 
+    // cast to FixLbMulticomponent * is it exists
     auto *fix = dynamic_cast<FixLbMulticomponent *>(fixes[0]);
     
+    // verify the cast to FixLbMulticomponent succeeded
     ASSERT_NE(fix, nullptr);
     
+    // obtain initial total momentum
     double jx0, jy0, jz0;
     fix->get_total_momentum(jx0, jy0, jz0);
     if (rank == 0)
         std::cout << "Intial P: " << jx0 << " " << jy0 << " " << jz0 << std::endl;
+    
+    // runs lammps time steps invoking fix_lb_multicomponent
     run_lammps(lmp); 
 
+    // obtain final total momentum
     double jx1, jy1, jz1;
     fix->get_total_momentum(jx1, jy1, jz1);
     
+    // verify conservation total momentum
     if (rank == 0){
       std::cout << "Final P: " << jx1 << " " << jy1 << " " << jz1 << std::endl;
       EXPECT_NEAR(jx1, jx0, 1e-6);
